6029e9057b92d588d8274dafa33e9c34b8361f11
AloneSweety <139615872+AloneSweety@users.noreply.github.com>
2024-12-07 18:40:25 +0000

    edit

--- public/gettable.php
+++ public/gettable.php
@@ -2,32 +2,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Search name</title>
+	<title></title>
 </head>
 <body>
-<?php
-require_once 'dbconf.php';
-require_once 'myfun.php';
-?>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-		<table>
-			<tr>
-				<td align="right"> Name:</td>
-				<td><input type="text" name="name" /></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Search" /></td>
-			</tr>
-		</table>
 
+	<?php
+//get the db connection file
+
+require_once 'dbconf.php';//(conf/dbconf.php) (folder/file)
+require_once 'myfun.php';
+
+PrintTable ("employee",$connect);
+
+//create a search form
+//write a function to search the books
+//echo $_SERVER['PHP_SELF']; //GET THE FILE NAME
+
+?>
+
+<form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET">
+	<table >
+		<tr >
+			<td align=right>Emp_Name: </td>
+			<td><input type="text" name="empname"/></td>
+		</tr>
+		<tr>
+			<td><input type="submit" value="Search"></td>
+		</tr>
+
+	</table>
+
+</form>
 <?php
-if (isset($_GET['name']) && $_GET['name'] != '') {
-	Searchname($_GET['name'],$connect);
+
+if (isset($_GET['empname']) && $_GET['empname'] != '') {
+	Searchemployee($_GET['empname'],$connect);
 }
-	
+
+//Searchemployee('Mark',$connect);
 ?>
 </body>
 </html>
